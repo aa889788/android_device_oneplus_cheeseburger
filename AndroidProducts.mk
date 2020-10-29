3008055f892278273e722023499b7faff5ada7d7
shxyke <shxyke@gmail.com>
2020-10-29 21:39:46 +0800

    cheeseburger: ify exthmui
    Signed-off-by: shxyke <shxyke@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cheeseburger.mk
+    $(LOCAL_DIR)/exthm_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cheeseburger-user \
-    lineage_cheeseburger-userdebug \
-    lineage_cheeseburger-eng
+    exthm_cheeseburger-user \
+    exthm_cheeseburger-userdebug \
+    exthm_cheeseburger-eng
